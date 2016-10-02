feda4923c9598165b3f3cbb796e32023a6605885
Jan Vítek <jan.vitek@virtualmaster.com>
2016-10-03 00:33:02 +0200

    Issue #10: mutliple users can be assigned to an order, added admin role

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -3,7 +3,7 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :order
 
   def increase_time seconds
-    self.update_attributes(spent_time: self.spent_time + seconds.to_f/3600)
+    self.update_attributes(spent_time: self[:spent_time] + seconds.to_f/3600)
   end
 
   def spent_time
